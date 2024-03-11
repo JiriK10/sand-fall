42b43d598b31f81a6d4f3447279d0fcad7dff5b1
JiriK10 <kasparek.jirka@gmail.com>
2024-03-11 18:35:15 +0100

    Settings more compact

---
+++ src/components/SettingCaption.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps({
+  text: String,
+  chip: String,
+})
+</script>
+
+<template>
+  <div class="text-caption flex items-center justify-center">
+    {{ text }}
+    <v-chip v-if="chip" label color="primary" size="small" class="ml-2">
+      {{ chip }}
+    </v-chip>
+  </div>
+</template>
--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -4,11 +4,13 @@ import { useSettingsStore } from "../stores/settings"
 import { useDesertStore } from "../stores/desert"
 import { useRuntimeStore } from "../stores/runtime"
 
+import SettingCaption from "./SettingCaption.vue"
+
 const settingsStore = useSettingsStore()
 const desertStore = useDesertStore()
 const runtimeStore = useRuntimeStore()
 
-const headerClass = "text-h5 mt-3 mb-1"
+const headerClass = "text-h5 mt-1"
 const switchAttrs = {
   "hide-details": true,
   color: "primary",
@@ -17,7 +19,7 @@ const sliderAttrs = {
   "thumb-label": true,
   "hide-details": true,
   color: "primary",
-  class: "pb-2",
+  class: "pb-1",
 }
 
 function trimSandDropClick() {
@@ -112,160 +114,8 @@ function presetColorStorm() {
 
 <template>
   <v-navigation-drawer permanent width="400" location="right">
-    <div class="p-4">
-      <div :class="headerClass">Desert</div>
-      <div class="text-caption">Width - {{ settingsStore.desertWidth }}px</div>
-      <v-slider
-        v-bind="sliderAttrs"
-        v-model="settingsStore.desertWidth"
-        min="50"
-        max="4000"
-        step="50"
-        :disabled="runtimeStore.isRunning"
-      />
-      <div class="text-caption">
-        Height - {{ settingsStore.desertHeight }}px
-      </div>
-      <v-slider
-        v-bind="sliderAttrs"
-        v-model="settingsStore.desertHeight"
-        min="50"
-        max="2000"
-        step="50"
-        :disabled="runtimeStore.isRunning"
-      />
-      <div :class="headerClass">
-        Sand ({{
-          (runtimeStore.isRunning && desertStore.moving.length) || "_"
-        }})
-      </div>
-      <div class="text-caption">Size - {{ settingsStore.sandSize }}px</div>
-      <v-slider
-        v-bind="sliderAttrs"
-        v-model="settingsStore.sandSize"
-        min="1"
-        max="12"
-        step="1"
-        :disabled="runtimeStore.isRunning"
-      />
-      <div class="text-caption">Speed - {{ settingsStore.sandSpeed }}ms</div>
-      <v-slider
-        v-bind="sliderAttrs"
-        v-model="settingsStore.sandSpeed"
-        min="25"
-        max="5000"
-        step="25"
-      />
-      <div class="text-caption">
-        Acceleration - {{ settingsStore.sandAcceleration }}ms
-      </div>
-      <v-slider
-        v-bind="sliderAttrs"
-        v-model="settingsStore.sandAcceleration"
-        min="0"
-        max="1000"
-        step="25"
-      />
-      <div class="text-caption">Coloration</div>
-      <v-btn-toggle
-        v-model="settingsStore.sandColor"
-        mandatory
-        color="primary"
-        variant="flat"
-        density="comfortable"
-        class="mb-2"
-      >
-        <v-btn :value="SandColor.Sand" :text="SandColor.Sand.toString()" />
-        <v-btn :value="SandColor.Grey" :text="SandColor.Grey.toString()" />
-        <v-btn :value="SandColor.Color" :text="SandColor.Color.toString()" />
-      </v-btn-toggle>
-      <div class="text-caption">
-        Coloration change during time - {{ settingsStore.sandColorChange }}%
-      </div>
-      <v-slider
-        v-bind="sliderAttrs"
-        v-model="settingsStore.sandColorChange"
-        min="0"
-        max="2"
-        step="0.01"
-      />
-      <div class="flex flex-row" :class="headerClass">
-        <span class="mr-8">Drop sand from top</span>
-        <v-switch
-          v-bind="switchAttrs"
-          v-model="settingsStore.sandDropTop"
-          density="compact"
-        />
-      </div>
-      <div class="text-caption">
-        Drop every {{ settingsStore.sandDropTopSpeed }}ms
-      </div>
-      <v-slider
-        v-bind="sliderAttrs"
-        v-model="settingsStore.sandDropTopSpeed"
-        min="0"
-        max="10000"
-        step="25"
-        :disabled="!settingsStore.sandDropTop"
-      />
-      <div :class="headerClass">Drop sand on click</div>
-      <div class="text-caption">Amount - {{ settingsStore.sandDropClick }}</div>
-      <v-slider
-        v-bind="sliderAttrs"
-        v-model="settingsStore.sandDropClick"
-        min="0"
-        :max="Math.pow(settingsStore.sandDropClickBox, 2)"
-        step="1"
-      />
-      <div class="text-caption">
-        Box - {{ settingsStore.sandDropClickBox }}x{{
-          settingsStore.sandDropClickBox
-        }}
-      </div>
-      <v-slider
-        v-bind="sliderAttrs"
-        v-model="settingsStore.sandDropClickBox"
-        @update:modelValue="trimSandDropClick()"
-        min="1"
-        max="5"
-        step="2"
-      />
-      <div :class="headerClass">Drop sand under cursor</div>
-      <div class="text-caption">
-        Amount - {{ settingsStore.sandDropCursor }}
-      </div>
-      <v-slider
-        v-bind="sliderAttrs"
-        v-model="settingsStore.sandDropCursor"
-        min="0"
-        :max="Math.pow(settingsStore.sandDropCursorBox, 2)"
-        step="1"
-      />
-      <div class="text-caption">
-        Box - {{ settingsStore.sandDropCursorBox }}x{{
-          settingsStore.sandDropCursorBox
-        }}
-      </div>
-      <v-slider
-        v-bind="sliderAttrs"
-        v-model="settingsStore.sandDropCursorBox"
-        @update:modelValue="trimSandDropCursor()"
-        min="1"
-        max="5"
-        step="2"
-      />
-      <div class="text-caption">
-        Speed - {{ settingsStore.sandDropCursorSpeed }}ms
-      </div>
-      <v-slider
-        v-bind="sliderAttrs"
-        v-model="settingsStore.sandDropCursorSpeed"
-        min="0"
-        max="10000"
-        step="25"
-      />
-      <v-divider />
-      <div class="my-4 flex justify-between">
+    <div class="p-4 text-center">
+      <div class="mb-3 flex justify-between">
         <v-btn
           prepend-icon="mdi-timer-sand"
           color="green-darken-1"
@@ -284,11 +134,179 @@ function presetColorStorm() {
           prepend-icon="mdi-eraser"
           color="primary"
           text="Clear"
+          :disabled="!runtimeStore.isRunning"
           @click="desertStore.clear"
         />
       </div>
-      <v-divider />
-      <div class="mt-4 flex flex-wrap justify-between gap-2">
+      <div :class="headerClass">Desert</div>
+      <SettingCaption text="Width" :chip="`${settingsStore.desertWidth}px`" />
+      <v-slider
+        v-bind="sliderAttrs"
+        v-model="settingsStore.desertWidth"
+        min="50"
+        max="4000"
+        step="50"
+        :disabled="runtimeStore.isRunning"
+      />
+      <SettingCaption text="Height" :chip="`${settingsStore.desertHeight}px`" />
+      <v-slider
+        v-bind="sliderAttrs"
+        v-model="settingsStore.desertHeight"
+        min="50"
+        max="2000"
+        step="50"
+        :disabled="runtimeStore.isRunning"
+      />
+      <div :class="headerClass">
+        Sand ({{
+          (runtimeStore.isRunning && desertStore.moving.length) || "_"
+        }})
+      </div>
+      <SettingCaption text="Size" :chip="`${settingsStore.sandSize}px`" />
+      <v-slider
+        v-bind="sliderAttrs"
+        v-model="settingsStore.sandSize"
+        min="1"
+        max="12"
+        step="1"
+        :disabled="runtimeStore.isRunning"
+      />
+      <SettingCaption text="Speed" :chip="`${settingsStore.sandSpeed}ms`" />
+      <v-slider
+        v-bind="sliderAttrs"
+        v-model="settingsStore.sandSpeed"
+        min="25"
+        max="5000"
+        step="25"
+      />
+      <SettingCaption
+        text="Acceleration"
+        :chip="`${settingsStore.sandAcceleration}ms`"
+      />
+      <v-slider
+        v-bind="sliderAttrs"
+        v-model="settingsStore.sandAcceleration"
+        min="0"
+        max="1000"
+        step="25"
+      />
+      <SettingCaption text="Coloration" />
+      <v-btn-toggle
+        v-model="settingsStore.sandColor"
+        mandatory
+        color="primary"
+        variant="flat"
+        density="comfortable"
+        class="mb-2"
+      >
+        <v-btn :value="SandColor.Sand" :text="SandColor.Sand.toString()" />
+        <v-btn :value="SandColor.Grey" :text="SandColor.Grey.toString()" />
+        <v-btn :value="SandColor.Color" :text="SandColor.Color.toString()" />
+      </v-btn-toggle>
+      <SettingCaption
+        text="Coloration change during time"
+        :chip="`${settingsStore.sandColorChange}%`"
+      />
+      <v-slider
+        v-bind="sliderAttrs"
+        v-model="settingsStore.sandColorChange"
+        min="0"
+        max="2"
+        step="0.01"
+      />
+      <div class="flex flex-row justify-center" :class="headerClass">
+        <span class="mr-6">Drop sand from top</span>
+        <v-switch
+          v-bind="switchAttrs"
+          v-model="settingsStore.sandDropTop"
+          density="compact"
+        />
+      </div>
+      <SettingCaption
+        text="Drop every"
+        :chip="`${settingsStore.sandDropTopSpeed}ms`"
+      />
+      <v-slider
+        v-bind="sliderAttrs"
+        v-model="settingsStore.sandDropTopSpeed"
+        min="0"
+        max="10000"
+        step="25"
+        :disabled="!settingsStore.sandDropTop"
+      />
+      <div :class="headerClass">Drop sand on click</div>
+      <div class="flex">
+        <div class="w-20">
+          <SettingCaption
+            text="Box"
+            :chip="`${settingsStore.sandDropClickBox}x${settingsStore.sandDropClickBox}`"
+          />
+          <v-slider
+            v-bind="sliderAttrs"
+            v-model="settingsStore.sandDropClickBox"
+            @update:modelValue="trimSandDropClick()"
+            min="1"
+            max="5"
+            step="2"
+          />
+        </div>
+        <div class="flex-grow">
+          <SettingCaption
+            text="Amount"
+            :chip="`${settingsStore.sandDropClick}`"
+          />
+          <v-slider
+            v-bind="sliderAttrs"
+            v-model="settingsStore.sandDropClick"
+            min="0"
+            :max="Math.pow(settingsStore.sandDropClickBox, 2)"
+            step="1"
+          />
+        </div>
+      </div>
+      <div :class="headerClass">Drop sand under cursor</div>
+      <div class="flex">
+        <div class="w-20">
+          <SettingCaption
+            text="Box"
+            :chip="`${settingsStore.sandDropCursorBox}x${settingsStore.sandDropCursorBox}`"
+          />
+          <v-slider
+            v-bind="sliderAttrs"
+            v-model="settingsStore.sandDropCursorBox"
+            @update:modelValue="trimSandDropCursor()"
+            min="1"
+            max="5"
+            step="2"
+          />
+        </div>
+        <div class="flex-grow">
+          <SettingCaption
+            text="Amount"
+            :chip="`${settingsStore.sandDropCursor}`"
+          />
+          <v-slider
+            v-bind="sliderAttrs"
+            v-model="settingsStore.sandDropCursor"
+            min="0"
+            :max="Math.pow(settingsStore.sandDropCursorBox, 2)"
+            step="1"
+          />
+        </div>
+      </div>
+      <SettingCaption
+        text="Speed"
+        :chip="`${settingsStore.sandDropCursorSpeed}ms`"
+      />
+      <v-slider
+        v-bind="sliderAttrs"
+        v-model="settingsStore.sandDropCursorSpeed"
+        min="0"
+        max="10000"
+        step="25"
+      />
+      <div :class="headerClass">Presets</div>
+      <div class="mt-1 flex flex-wrap justify-between gap-2">
         <v-btn color="primary" text="Dunes" @click="presetDunes" />
         <v-btn color="amber-lighten-3" text="Relax" @click="presetRelax" />
         <v-btn
